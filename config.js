9fbdf32e709d3a0dcfb4454a62cc0bedaf4dbf47
MadakaHeri <madakaheri@icloud.com>
2026-02-02 15:10:46 +0900

    config修正

--- config.js
+++ config.js
@@ -1,4 +1,31 @@
 
+/**
+ * SDKビルド設定
+ * @type {{srcPath: string, outPath: string, serviceFunctionName?: string}}
+ */
+export const sdkConfig = {
+	srcPath: new URL('test-repo/api/src/actions', import.meta.url).pathname,
+	outPath: new URL('test-repo/sdk/src/actions', import.meta.url).pathname,
+	// serviceFunctionName: 'lambda-function-name',
+};
+
+/**
+ * @deprecated sdkConfig.srcPathへ移行
+ * APIのアクションディレクトリパス
+ * @type {string}
+ */
 export const apiActionsDirectory = new URL('test-repo/api/src/actions', import.meta.url).pathname;
+
+/**
+ * @deprecated sdkConfig.outPathへ移行
+ * 自動更新するSDKのアクションディレクトリパス
+ * @type {string}
+ */
 export const sdkActionsDirectory = new URL('test-repo/sdk/src/actions', import.meta.url).pathname;
 
+/**
+ * @deprecated sdkConfig.serviceFunctionNameへ移行
+ * サービスのLambda関数名
+ * @type {string | undefined}
+ */
+export const serviceFunctionName = 'test-function';
